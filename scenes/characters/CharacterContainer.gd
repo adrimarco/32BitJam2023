8e6457064af94ed788b84baf01d8a30d7859676d
adrimarco <adrianmarco-@hotmail.es>
2023-12-22 11:08:22 +0100

    feat: add boss fight

--- scenes/characters/CharacterContainer.gd
+++ scenes/characters/CharacterContainer.gd
@@ -1,9 +1,12 @@
 class_name CharacterContainer
 extends Node
 
+static var BOSS_OFFSET	:= 100
+
 var characters 			:Array
 var bosses				:Array
 var teams				:Array[Array]
+var boss_teams			:Array[Array]
 var teams_count			:int
 
 func _ready():
@@ -18,14 +21,24 @@ func _ready():
 	teams.append([2, 1, 1])
 	teams.append([0, 1, 3])
 	teams.append([4, 1, 3])
-	
 	teams_count = teams.size()
+	
+	boss_teams.append([BOSS_OFFSET])
 
 func get_character_scene_by_id(id:int) -> PackedScene:
+	if id >= BOSS_OFFSET:
+		return get_boss_by_id(id - BOSS_OFFSET)
+	
 	if id < 0 or id >= characters.size():
 		return null
 	
 	return characters[id]
+
+func get_boss_by_id(id:int) -> PackedScene:
+	if id < 0 or id >= bosses.size():
+		return null
+	
+	return bosses[id]
 
 func get_team_by_id(id:int) -> Array[int]:
 	if id < 0 or id >= teams_count:
@@ -33,4 +46,9 @@ func get_team_by_id(id:int) -> Array[int]:
 	
 	var team :Array[int] = []
 	team.append_array(teams[id])
+	return team
+
+func get_boss_team() -> Array[int]:
+	var team :Array[int] = []
+	team.append_array(boss_teams[0])
 	return team
